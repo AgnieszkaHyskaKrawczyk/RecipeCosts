42ffd3a9f0120df2dc25d0780257f6d10b17eb25
Agnieszka <agnieszka.krawczyk@seznam.cz>
2019-05-21 21:59:41 +0200

    people -changes

--- Students/Students.py
+++ Students/Students.py
@@ -3,9 +3,25 @@ file = open('absolventi.json', encoding='utf-8')
 text = file.read()
 absolventi = json.loads(text)
 print(absolventi)
+print(len(absolventi))
 ####
 import requests
 import json
 response = requests.get('http://api.kodim.cz/python-data/people')
 data = json.loads(response.text)
 print(data)
+print(len(data))
+#for item in data:
+for item in data[0]:
+    print(item)
+sumMale = 0
+sumFemale = 0
+for man in data:
+    #print(man['gender'])
+    if man['gender'] == 'Male':
+        sumMale = sumMale + 1
+    else:
+        sumFemale = sumFemale + 1
+
+print('Men:    ', sumMale)
+print('Female: ', sumFemale)
